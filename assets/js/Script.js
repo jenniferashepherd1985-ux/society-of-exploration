fb5dadba28f50a1e3541a3496782f561c4605a2e
jenniferashepherd1985-ux <jennifer.a.shepherd1985@gmail.com>
2026-08-07 21:39:17 +0100

    Update Script.js

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -1,14 +1,14 @@
+let assignments = [];
 document.addEventListener("DOMContentLoaded", () => {
 
     fetch("data/assignments.json")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            alert("Assignments loaded successfully!");
-        })
-        .catch(error => {
-            console.error(error);
-            alert("Couldn't load assignments.json");
-        });
+    .then(response => response.json())
+    .then(data => {
+        assignments = data.assignments;
+        console.log(assignments);
+    })
+    .catch(error => {
+        console.error(error);
+    });
 
 });
